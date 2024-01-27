b2f9de552176acddfa006f03fedda5aa15142520
owino600 <owinodaniel600@gmail.com>
2024-01-27 11:14:18 +0300

    hospital updates

--- Hospital.py
+++ Hospital.py
@@ -14,41 +14,41 @@ class Hospital:
             print("welcome patient!")
             #continue
         
-        class User_credentials:
-            def __init__(self, username, email, password):
-                self.username=username
-                self.email=email
-                self.password=password
-                user = user_credentials(username, email, password)
-                if email in user_credentials == password:
-                    print('account exist, logged in successfully!')
-                else:
-                    user_credentials[email, username] = password
-                    while true:
-                        print("welcome to your system")
-                        print("1.")
-                        print("2.")
-                        choice = input("Enter choice")
-                        if choice == "1":
-                            username = input("ENTER USERNAME: ")
-                            email = input("ENTER EMAIL ADDRESS: ")
-                            password = input("ENTER PASSWORD: ")
-                            if login(username, email, password):
-                                print("Login successful!")
-                            break
-                        else:
-                            print("acount unavailable, please sign up!")
-                            user_credentials[email, username] = password
+class User_credentials:
+    def __init__(self, username, email, password):
+        self.username=username
+        self.email=email
+        self.password=password
+        user = user_credentials(username, email, password)
+        if email in user_credentials == password:
+            print('account exist, logged in successfully!')
+        else:
+            user_credentials[email, username] = password
+            while true:
+                print("welcome to your system")
+                print("1.")
+                print("2.")
+                choice = input("Enter choice")
+        if choice == "1":
+            username = input("ENTER USERNAME: ")
+            email = input("ENTER EMAIL ADDRESS: ")
+            password = input("ENTER PASSWORD: ")
+        if login(username, email, password):
+            print("Login successful!")
+            break
+        else:
+            print("acount unavailable, please sign up!")
+            user_credentials[email, username] = password
 
-                            class Doctor:
-                                def __init__(self, prescribe, admit, release):
-                                    self.prescribe = prescribe
-                                    self.admit = admit
-                                    self.release = release
-                                    patient = Doctor(prescribe, admit, release)
-                                    if patient == prescribed:
-                                        print('Please prescribe the required drugs')
-                                        prescribe = input("")
-                                    elif patient == admit:
-                                        print('Enter reson to admit')
-                                        admit = input("")
+class Doctor:
+    def __init__(self, prescribe, admit, release):
+        self.prescribe = prescribe
+        self.admit = admit
+        self.release = release
+        patient = Doctor(prescribe, admit, release)
+        if patient == prescribed:
+            print('Please prescribe the required drugs')
+            prescribe = input("")
+        elif patient == admit:
+            print('Enter reson to admit')
+            admit = input("")
